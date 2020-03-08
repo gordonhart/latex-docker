bd03a158363402726149b7a18522cd4cc45cbec5
Gordon Hart <gordon.hart2@gmail.com>
2020-03-07 23:00:16 -0800

    Update latexdockercmd.sh to properly set UID:GID to user on docker-machine VM

--- latexdockercmd.sh
+++ latexdockercmd.sh
@@ -1,3 +1,17 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -eu
+
 IMAGE=blang/latex:ubuntu
-exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
+ACTIVE_DOCKER_MACHINE="$(docker-machine active)"
+UID_GID=$(docker-machine ssh $ACTIVE_DOCKER_MACHINE 'echo "$(id -u):$(id -g)"')
+
+exec docker run \
+    --rm \
+    --interactive \
+    --tty \
+    --user "$UID_GID" \
+    --network none \
+    --mount type=bind,src="$PWD",dst=/data \
+    "$IMAGE" \
+    "$@"
