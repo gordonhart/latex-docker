c1f4f86814eecdf4d43dd7b4fbb89c4730518c7c
Gordon Hart <gordon.hart2@gmail.com>
2021-02-11 21:16:12 -0500

    De-docker-machine-ify

--- latexdockercmd.sh
+++ latexdockercmd.sh
@@ -3,15 +3,12 @@
 set -eu
 
 IMAGE="blang/latex:ubuntu"
-ACTIVE_DOCKER_MACHINE="$(docker-machine active)"
-UID_GID="$(docker-machine ssh $ACTIVE_DOCKER_MACHINE 'echo "$(id -u):$(id -g)"')"
 
 exec docker run \
     --rm \
     --interactive \
     --tty \
     --network none \
-    --user "$UID_GID" \
     --mount type=bind,src="$PWD",dst=/data \
     "$IMAGE" \
     "$@"
